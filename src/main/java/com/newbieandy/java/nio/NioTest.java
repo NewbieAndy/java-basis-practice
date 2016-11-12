3fabddd4df8bb18e2d3f341a58d64efd5789bf76
machao <chao.ma@mtime.com>
2016-11-12 15:33:29 +0800

    reflect test

--- src/main/java/com/newbieandy/java/nio/NioTest.java
+++ src/main/java/com/newbieandy/java/nio/NioTest.java
@@ -1,7 +1,37 @@
 package com.newbieandy.java.nio;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /**
  * Created by mchao on 2016/11/12.
  */
 public class NioTest {
+    public static void main(String[] args) throws IOException {
+//        StringBuffer buffer = new StringBuffer();
+
+        String path = System.getProperty("user.dir") + "\\src\\main\\resources\\trywithresource\\";
+        String sourcePath = path + "niotest.txt";
+        String goalPath = path + "niotest1.txt";
+
+        FileInputStream fileInputStream = new FileInputStream(new File(sourcePath));
+        FileOutputStream fileOutputStream = new FileOutputStream(new File(goalPath));
+        FileChannel inChannel = fileInputStream.getChannel();
+        FileChannel outChannel = fileOutputStream.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        while (true) {
+            buffer.clear();
+            int read = inChannel.read(buffer);
+            if (read == -1) {
+                break;
+            }
+            buffer.flip();
+            outChannel.write(buffer);
+        }
+
+    }
 }
--- src/main/java/com/newbieandy/java8/localdatetime/LocalDateTimeTest.java
+++ src/main/java/com/newbieandy/java8/localdatetime/LocalDateTimeTest.java
@@ -1,7 +1,25 @@
 package com.newbieandy.java8.localdatetime;
 
+import java.time.LocalDateTime;
+
 /**
  * Created by mchao on 2016/11/3.
  */
 public class LocalDateTimeTest {
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1() {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now + "now");
+        LocalDateTime dateTime = now.plusMinutes(1);
+        System.out.println(dateTime + "plusMin");
+        LocalDateTime dateTime2 = now.plusSeconds(1);
+        System.out.println(dateTime2 + "plussec");
+        LocalDateTime dateTime1 = now.plusNanos(1000);
+        System.out.println(dateTime1 + "plusnanos");
+        boolean after = dateTime.isAfter(now);
+        System.out.println(after);
+    }
 }
